c45fd3177b9c2a50919e25b2f6ab0a6eec8e9c55
Tanapan Panpak <64010323@kmitl.ac.th>
2021-09-04 11:57:32 +0700

    Add project files.

---
+++ Week4.2/WEEK4.2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main() {
+	int n,dist=1;
+	double f = 500;
+	double remain;
+	printf("Enter weight(Ton) : ");
+	scanf_s("%d", &n);
+	remain = (n * 1000)-500;
+
+	while (remain > 0) {
+		dist = dist + 1;
+		if (dist % 10 == 0 ) { 
+			remain = remain + 1000; 
+		}
+		else {
+			f = f + (f * (5.0 / 100));
+			remain = remain - f;
+		}	
+	}
+	printf("Distance = %d Kilometer", dist);
+	return 0;
+}
